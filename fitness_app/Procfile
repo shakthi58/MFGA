3239f67b3369b73b75f787d44fdaa3a2df9ca17f
shakthi58 <kg.shakthiganesh@gmail.com>
2026-07-20 09:24:55 +0530

    Added Gunicorn for railway deployment

---
+++ fitness_app/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fitness_app.wsgi:application --bind 0.0.0.0:$PORT
--- fitness_app/fitness_app/settings.py
+++ fitness_app/fitness_app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c4p@6!wi2ztx(dro7p9o8yo*6wz-ye2fnd&xotb%_vxxl4=tt0'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-c4p@6!wi2ztx(dro7p9o8yo*6wz-ye2fnd&xotb%_vxxl4=tt0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
